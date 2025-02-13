31be34b242cf4d34800aed530fee94c509f68d0f
Latch Jack <latch.jack@gmail.com>
2025-02-13 23:47:33 +0000

    [feat/exercise-endpoint] Added exercise-view page

--- backend/routes/exercise-routes.js
+++ backend/routes/exercise-routes.js
@@ -1,14 +1,28 @@
 import express from "express";
 const router = express.Router();
-import exercises from "..//data/exercises.js";
+// import exercises from "..//data/exercises.js";
+import asyncHandler from "../middleware/async-handler.js";
+import Exercise from "../models/exercise-model.js";
 
-router.get("/", (req, res) => {
-  res.json(exercises);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const exercises = await Exercise.find({});
+    res.json(exercises);
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const exercise = exercises.find((w) => w._id === req.params.id);
-  res.json(exercise);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const exercise = await Exercise.findById(req.params.id);
+    if (exercise) {
+      res.json(exercise);
+    }
+    res.status(404).json({ message: "Exercise not found" });
+    // const exercise = exercises.find((w) => w._id === req.params.id);
+    res.json(exercise);
+  })
+);
 
 export default router;
