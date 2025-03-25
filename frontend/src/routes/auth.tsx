87f13575e555ffd2f8b150e50e44701ed17bc599
Latch Jack <latch.jack@gmail.com>
2025-03-25 23:06:51 +0000

    [feat/frontend-auth] Added navigation on auth, and redirection if already authed.

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -1,6 +1,12 @@
 import { FormProvider, useForm } from "react-hook-form";
-import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Link,
+  useLocation,
+  useNavigate,
+} from "@tanstack/react-router";
 import useAuth from "../api/use-auth";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/auth")({
   component: RouteComponent,
@@ -13,19 +19,32 @@ type FormData = {
 
 function RouteComponent() {
   const methods = useForm<FormData>();
-  const { data, mutate } = useAuth();
+  const { mutate } = useAuth();
+  const goTo = useNavigate();
+  const location = useLocation();
+  const sp = new URLSearchParams(location.search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (userData) {
+      goTo({ to: redirect });
+    }
+  }, [goTo, redirect]);
 
   const onSubmit = (formData: FormData) => {
     mutate(formData, {
+      onError: (err) => {
+        console.log(err);
+        // add toast
+      },
       onSuccess: (res) => {
         console.log(res);
+        localStorage.setItem("user", JSON.stringify(res));
+        goTo({ to: redirect });
       },
     });
   };
-
-  if (data) {
-    localStorage.setItem("user", JSON.stringify(data));
-  }
 
   return (
     <div>
@@ -34,12 +53,19 @@ function RouteComponent() {
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-2">
             <div>
-              <label>Email</label>
-              <input placeholder="email" {...methods.register("email")} />
+              <label>
+                Email
+                <input placeholder="email" {...methods.register("email")} />
+              </label>
             </div>
             <div>
-              <label>Password</label>
-              <input placeholder="password" {...methods.register("password")} />
+              <label>
+                Password
+                <input
+                  placeholder="password"
+                  {...methods.register("password")}
+                />
+              </label>
             </div>
           </div>
           <button type="submit">Login</button>
